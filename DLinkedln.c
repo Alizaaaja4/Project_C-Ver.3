92b4c74f4c38cec332f06fe6d7f1b093865b8df9
Allllllllllll <137359388+Alizaaaja4@users.noreply.github.com>
2024-03-03 04:59:07 +0700

    First Commits D'Linkedln
    + added library
    + added function
    + added file

---
+++ DLinkedln.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void ban_DLinkedln();
+void welcome();
+    void dash_admin();
+        void signIn_admin();
+            void add_lowongan();
+            void delate_lowongan();
+            void list_lowongan();
+            void dash_akun();
+                void akun_prem();
+                    void approve_prem();
+                void akun_norm();
+            
+    void dash_user();
+            void signIn_user();
+            void signUp_user();
+                void  view_profile();
+                    void edit_profile();
+                void topUp_saldo();
+                void search_user();
+                void search_lowongan(); // disini bisa langsung daftar pekerjaan
+                void list_lowongan();
+                void upgrade_akun_prem();
+
